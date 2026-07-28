4fb09ed4e673a9eec857684ccd828ab0c8a92a9e
Mac World Technology <164502099+macworldgithub@users.noreply.github.com>
2026-07-29 00:23:58 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,12 +17,13 @@ connectDB();
 const app = express();
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     credentials: true,
+//   }),
+// );
+app.use(cors());
 app.use(morgan("dev"));
 
 // Swagger UI
